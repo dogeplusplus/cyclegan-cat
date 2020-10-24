7adbac6ef01470fe36167282416a727a70184edc
Albert Chung <albertswchung@gmail.com>
2020-10-24 14:25:58 +0100

    Changing fixtures for resnet tests.

--- unittests/test_resnet.py
+++ unittests/test_resnet.py
@@ -4,16 +4,25 @@ import numpy as np
 from cyclegan.resnet import resnet_generator, ReflectionPadding2D, simple_discriminator
 
 @pytest.fixture
-def config():
+def resnet_config():
     model_config = dict(filters=16)
+    return model_config
+
+@pytest.fixture
+def simple_config():
+    model_config = dict(
+        filters=[8, 16, 32],
+        kernels=[4, 4, 4],
+        normalization='instancenorm'
+    )
     return model_config
 
 @pytest.fixture
 def mock_image():
     return np.ones((1, 128, 128, 3))
 
-def test_resnet(config, mock_image):
-    model = resnet_generator(config)
+def test_resnet(resnet_config, mock_image):
+    model = resnet_generator(resnet_config)
     prediction = model(mock_image)
 
     assert prediction.shape == mock_image.shape
@@ -38,12 +47,7 @@ def test_reflection_padding():
     assert np.array_equal(expected, actual)
 
 
-def test_simple_discriminator(mock_image):
-    model_config = dict(
-        filters=[8, 16, 32],
-        kernels=[4, 4, 4],
-        normalization='instancenorm'
-    )
-    model = simple_discriminator(model_config)
+def test_simple_discriminator(simple_config, mock_image):
+    model = simple_discriminator(simple_config)
     prediction = model(mock_image).numpy()
     assert prediction.shape == (1, 16, 16, 1)
