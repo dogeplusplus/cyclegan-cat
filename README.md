86a73e312d7d7dd92f42c3bed1382439ec4429cb
dogeplusplus <albertswchung@gmail.com>
2022-01-09 13:22:37 +0000

    Update streamlit script, and README with slightly better example.

--- README.md
+++ README.md
@@ -2,9 +2,4 @@
 - Style transfer from brown tabby cats to tortoiseshell.
 - Support for UNet/ResNet generators and UNet/Simple CNN discriminators
 
-TODO
-1. Improve style transfer in general
-2. Support for arbitrary image sizes when predicting
-3. Optimizations to data pipeline
-
-![](example.png)
+![](tabby2tortie_demo.png)
--- predict.py
+++ predict.py
@@ -6,16 +6,16 @@ from pathlib import Path
 
 import tensorflow as tf
 
-from cyclegan.model import CycleGan
 from transform.data_load import normalize
-from model_processing.load_model import yaml2namespace
-
 
 def load_model():
-    model_config_path = Path("model_instances", "model", "model_config.yaml")
-    train_config_path = Path("model_instances", "model", "train_config.yaml")
-    model = CycleGan(yaml2namespace(str(model_config_path)), yaml2namespace(str(train_config_path)))
-    return model
+    model_ab_path = Path("model_instances", "model", "g_AB")
+    model_ba_path = Path("model_instances", "model", "g_BA")
+
+    model_ab = tf.keras.models.load_model(model_ab_path)
+    model_ba = tf.keras.models.load_model(model_ba_path)
+
+    return model_ab, model_ba
 
 def preprocess_image(image: np.array, size: Tuple[int, int]) -> np.array:
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -26,13 +26,13 @@ def preprocess_image(image: np.array, size: Tuple[int, int]) -> np.array:
 def postprocess_prediction(prediction: np.array):
     return np.array((prediction[0] + 1) * 127.5, np.uint8)
 
-def generate_prediction_triple(image, model):
+def generate_prediction_triple(image, model_ab, model_ba):
     IMAGE_SIZE = (256, 256)
     image_input = preprocess_image(image, IMAGE_SIZE)
-    prediction_ab = model.g_AB(image_input)
+    prediction_ab = model_ab(image_input)
     prediction_ab = postprocess_prediction(prediction_ab)
 
-    prediction_ba = model.g_BA(image_input)
+    prediction_ba = model_ba(image_input)
     prediction_ba = postprocess_prediction(prediction_ba)
 
     image_viz = cv2.resize(cv2.cvtColor(image, cv2.COLOR_BGR2RGB), IMAGE_SIZE)
@@ -40,7 +40,7 @@ def generate_prediction_triple(image, model):
 
 
 def main():
-    model = load_model()
+    tabby2tortie, tortie2tabby = load_model()
 
     st.title("Tabby2Tortie")
     tabby_upload = st.sidebar.file_uploader("Tabby Cat", type=["jpg", "jpeg", "png"])
@@ -52,14 +52,14 @@ def main():
 
     if tabby_upload:
         tabby_image = cv2.imdecode(np.fromstring(tabby_upload.getvalue(), np.uint8), cv2.IMREAD_COLOR)
-        image, tortie, tabby = generate_prediction_triple(tabby_image, model)
+        image, tortie, tabby = generate_prediction_triple(tabby_image, tabby2tortie, tortie2tabby)
         image_col.image(image)
         tabby_col.image(tabby)
         tortie_col.image(tortie)
 
     if tortie_upload:
         tortie_image = cv2.imdecode(np.fromstring(tortie_upload.getvalue(), np.uint8), cv2.IMREAD_COLOR)
-        image, tortie, tabby = generate_prediction_triple(tortie_image, model)
+        image, tortie, tabby = generate_prediction_triple(tortie_image, tabby2tortie, tortie2tabby)
         image_col.image(image)
         tabby_col.image(tabby)
         tortie_col.image(tortie)
